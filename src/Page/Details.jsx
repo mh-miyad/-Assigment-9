7dc1243c50fac255e66a362f7b16a8f2c4f52bed
miyad <mhmiyad6565@gmail.com>
2023-04-10 22:58:52 +0600

    details page show data

--- src/Page/Details.jsx
+++ src/Page/Details.jsx
@@ -18,9 +18,29 @@ const Details = () => {
     return (
         <div>
             <JobHeader/>
-            <div>
+           <div className='container mx-auto '>
+           <div className='flex  justify-around'>
+                     <div className=' border w-1/2'> 
+                        <p className=' text-xl lg:text-2xl font-bold'> Job Description : <span className=' text-sm lg:text-lg font-normal'> {existingData.job_description}</span></p>
+                        <p className=' text-xl lg:text-2xl font-bold'> Job Responsibility: <span className=' text-sm lg:text-lg font-normal'> {existingData.job_responsibility}</span></p>
+                      
+                     </div>
+                     <div className='border  bg-fuchsia-500/20 p-10'> 
 
+
+                        <div>
+                        Job Details
+
+                        </div>
+                        <hr  className='h-0.5 my-5 bg-slate-600'/>
+                        <div>
+                          
+                           <p> $ salary: {existingData.salary}</p>
+                           <p> <span className='font-bold'> Job Title</span>:{existingData.job_title}</p>
+                        </div>
+                     </div>
             </div>
+           </div>
         </div>
     )
 }
