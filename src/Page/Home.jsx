c24d8d4ed7ddb73d50aaa1574528ec7079eb907d
miyad <mhmiyad6565@gmail.com>
2023-04-10 19:21:03 +0600

    Show more button Add here

--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from '../Component/Header'
 import CatagoryJob from '../Component/CatagoryJob'
 import CartComponent from '../Component/CartComponent'
  import data from '../Data/jobs.json'
 const Home = () => {
+
+  const [ showMore , setShowMore] = useState(false)
+  const toggleShowMore = () => {
+    setShowMore(!showMore);
+  };
+  const visibleData = showMore ? data : data.slice(0, 4);
+
   return (
     <div className='container mx-auto  '>
          <Header/>
@@ -11,12 +18,19 @@ const Home = () => {
     <div>
        <p className='text-center text-2xl md:text-5xl my-5'>Featured Jobs</p>
        <p className='text-center text-sm md:text-lg mb-5'>Explore thousands of job opportunities with all the information you need. Its your future</p>
-    <div className=' grid grid-cols-1 md:grid-cols-2'>
+   <div className='text-center'>
+   <div className=' grid grid-cols-1 md:grid-cols-2 text-start'>
         
-        { data.map((ele)=><CartComponent img={ele.company_logo} jobTitle={ele.job_title} location={ele.fulltime_or_parttime}
+        { visibleData.map((ele)=><CartComponent img={ele.company_logo} jobTitle={ele.job_title} location={ele.fulltime_or_parttime}
         remote={ele.remote_or_onsite} salry={ele.salary} place={ele.location}/>)}
         </div>
-  
+        <button
+          onClick={toggleShowMore}
+          className="my-5 rounded-md  font-bold text-white bg-btn py-3 px-5 shadow-2xl shadow-red-600/20 "
+        >
+          {showMore ? "Show less" : "Show more"}
+        </button>
+   </div>
     </div>
     </div>
   )
