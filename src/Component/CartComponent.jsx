72f1626aa9271dd6d599b964bde083ff4dc81515
Mahamudul Hasan Miyad <mhmiyad6565@gmail.com>
2023-04-10 12:49:43 +0600

    card show

--- src/Component/CartComponent.jsx
+++ src/Component/CartComponent.jsx
@@ -7,19 +7,19 @@ const CartComponent = () => {
   return (
     <div className='mx-5'>
          <div className='container mx-auto '>
-           <div className='border p-5 my-5 space-y-2'>
+           <div className='border p-5 my-5 space-y-3 rounded-lg shadow'>
             <img src='https://i.ibb.co/nm14Wjc/google-1-1-1-1.png' alt="" />
             <p>Technical Database Engineer</p>
             <div className='flex justify-evenly'>
               <ButtonCom/>
               <ButtonCom/>
             </div>
-            <div className='flex gap-3'>
-              <img src={logo} alt="" />
+            <div className='flex gap-'>
+              <img src={logo} alt="" className='w-5 h-5 sm:w-auto sm:h-auto' />
               <p> Dhaka Bangladesh </p>
               <p>  $ salary:100k-300k </p>
             </div>
-            <Button className='bg-btn'> View Details </Button>       
+            <Button className='bg-btn '> View Details </Button>       
                 </div>
 
 
--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -2,15 +2,21 @@ import React from 'react'
 import Header from '../Component/Header'
 import CatagoryJob from '../Component/CatagoryJob'
 import CartComponent from '../Component/CartComponent'
-
+ import data from '../Data/jobs.json'
 const Home = () => {
   return (
     <div className='container mx-auto  '>
          <Header/>
       <CatagoryJob/>
-
-      <CartComponent/>
-
+    <div>
+       <p className='text-center text-2xl md:text-5xl my-5'>Featured Jobs</p>
+       <p className='text-center text-sm md:text-lg mb-5'>Explore thousands of job opportunities with all the information you need. Its your future</p>
+    <div className=' grid grid-cols-1 md:grid-cols-2'>
+        
+        { data.map((ele)=><CartComponent/>)}
+        </div>
+  
+    </div>
     </div>
   )
 }
